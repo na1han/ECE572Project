a4d30ee8b6b25ee9751c8eb59c083683edfc9603
Nathan <saliga.nathan@gmail.com>
2014-02-13 14:09:26 -0500

    Removed the "\n" to signal serial complete
    Now the control will update itself every time it receives new data from the computer.  This will allow the control algorithm to update more than the 30 fps that the computer is feeding it.

--- FinalProjectMain/FinalProjectMain.ino
+++ FinalProjectMain/FinalProjectMain.ino
@@ -1,7 +1,5 @@
 #include <Servo.h> 
 
-String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
 int xTarget;                     // Target location for the ball from the computer
 int yTarget;                    
 int xLoc;                        // Actual loction of the ball from the computer
@@ -20,8 +18,7 @@ void setup() {
   // initialize serial:
   Serial.begin(9600);
   // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
-  
+  inputString.reserve(20);
   xServo.attach(9);  // attaches the servo on pin 9 to the servo object
   yServo.attach(8);  // attaches the servo on pin 8 to the servo object
   xResetAngle = 90;
@@ -31,33 +28,21 @@ void setup() {
 }
 
 void loop() {
-  // print the string when a newline arrives:
-  if (stringComplete) {
-    Serial.println(inputString); 
-    // clear the string:
-    inputString = "";
-    stringComplete = false;
-    if(isBallOnTable) {
-      //TODO PID corrections
-      
-      xAngle = xAngle + xCorrection;
-      yAngle = yAngle + yCorrection;
-      xServo.write(constrain(xAngle, 75, 105)); // Only allowing 15 degrees of correction for now 
-      yServo.write(constrain(yAngle, 75, 105)); // Only allowing 15 degrees of correction for now
-    }
-    else {
-      xServo.write(xResetAngle); // Set table to flat if ball is not present
-      yServo.write(yResetAngle); // Set table to flat if ball is not present
-    }
+  if(isBallOnTable) {
+  
+    //TODO PID corrections
+    
+    xAngle = xAngle + xCorrection;
+    yAngle = yAngle + yCorrection;
+    xServo.write(constrain(xAngle, 75, 105)); // Only allowing 15 degrees of correction for now 
+    yServo.write(constrain(yAngle, 75, 105)); // Only allowing 15 degrees of correction for now
+  }
+  else {
+    xServo.write(xResetAngle); // Set table to flat if ball is not present
+    yServo.write(yResetAngle); // Set table to flat if ball is not present
   }
 }
 
-/*
-  SerialEvent occurs whenever a new data comes in the
- hardware serial RX.  This routine is run between each
- time loop() runs, so using delay inside loop can delay
- response.  Multiple bytes of data may be available.
- */
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
@@ -78,9 +63,6 @@ void serialEvent() {
     else if (inChar == 'b') {          // 'b' transmission means the next byte will be a 1 or 0 whether the ball is there or not
       isBallOnTable = (int)Serial.read();
     }
-    else if (inChar == '\n') {             // '\n' means that all the data has been loaded and control can begin
-      stringComplete = true;
-    } 
   }
 }
 
