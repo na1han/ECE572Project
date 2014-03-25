c0fb71c35840d8d2b82af7c05feb257f301371ba
Lohrer <michael.f.lohrer@gmail.com>
2014-03-24 22:03:32 -0400

    Initial control tuning, velocity look-ahead

--- FinalProjectMain/FinalProjectMain.ino
+++ FinalProjectMain/FinalProjectMain.ino
@@ -1,12 +1,6 @@
 #include <Average.h>
 #include <Servo.h> 
-#include "LedControl.h"
 
-/***** These pin numbers will probably not work with your hardware *****
-pin 12 is connected to the DataIn 
-pin 11 is connected to the CLK 
-pin 10 is connected to LOAD  */ 
-LedControl lc=LedControl(12,11,10,2);
 int xTarget;                     // Target location for the ball from the computer
 int yTarget;                    
 int xLoc;                        // Actual loction of the ball from the computer
@@ -18,7 +12,7 @@ int yResetAngle;
 int isBallOnTable = 1;                // 1 if ball is present 0 if not
 Servo xServo;  // create servo object to control the rotation around the x axis
 Servo yServo;  // create servo object to control the rotation around the x axis
-float pGain = .01;
+float pGain = .1;
 float iGain = .01;
 float dGain = 1;
 float dBuffer[] = {0, 0, 0, 0};
@@ -27,6 +21,8 @@ word data = 0;
 int led = 13;
 int dataPiece = 0;
 int BoolRX = 0;
+int BoolNewData = 0;
+int maxAngle = 15;
 
 
 void setup() {
@@ -34,43 +30,33 @@ void setup() {
   Serial.begin(115200);
   xServo.attach(9);  // attaches the servo on pin 9 to the servo object
   yServo.attach(8);  // attaches the servo on pin 8 to the servo object
-  xResetAngle = 90;
-  yResetAngle = 85;
+  xResetAngle = 92;
+  yResetAngle = 87;
   xAngle = xResetAngle; 
   yAngle = yResetAngle;
   xServo.write(xResetAngle); // Set table to flat if ball is not present
   yServo.write(yResetAngle); // Set table to flat if ball is not present
   isBallOnTable = 0;
-  
-  int devices=lc.getDeviceCount();
-  for(int address=0;address<devices;address++) {
-    /*The MAX72XX is in power-saving mode on startup*/
-    lc.shutdown(address,false);
-    /* Set the brightness to a medium values */
-    lc.setIntensity(address,6);
-    /* and clear the display */
-    lc.clearDisplay(address);
-  }
 }
 
 void loop() {
     //writeDisplay();
     //run control structure if the ball is present
-    if(isBallOnTable) {
-      xAngle = constrain(xAngle - PID(xLoc - xTarget), 65, 115);
-      yAngle = constrain(yAngle + PID(yLoc - yTarget), 65, 115);
-      //xAngle = constrain(xAngle - (xLoc - xTarget), 65, 115);
-      //yAngle = constrain(yAngle + (yLoc - yTarget), 65, 115);
-      xServo.write(xAngle); // Only allowing 15 degrees of correction for now 
-      yServo.write(yAngle); // Only allowing 15 degrees of correction for now
+    if(BoolNewData) {
+      BoolNewData = 0;
+      if(isBallOnTable) {
+        xAngle = constrain(xAngle - PID(xLoc - xTarget), xResetAngle-maxAngle, xResetAngle+maxAngle);
+        yAngle = constrain(yAngle + PID(yLoc - yTarget), yResetAngle-maxAngle, yResetAngle+maxAngle);
+        xServo.write(xAngle); // Only allowing 15 degrees of correction for now 
+        yServo.write(yAngle); // Only allowing 15 degrees of correction for now
+      }
+      else {
+        xServo.write(xResetAngle); // Set table to flat if ball is not present
+        yServo.write(yResetAngle); // Set table to flat if ball is not present
+        xAngle = xResetAngle;
+        yAngle = yResetAngle;
+      }
     }
-    else {
-      xServo.write(xResetAngle); // Set table to flat if ball is not present
-      yServo.write(yResetAngle); // Set table to flat if ball is not present
-      xAngle = xResetAngle;
-      yAngle = yResetAngle;
-    }
-    delay(10);
 }
 
 int PID(float error) {
@@ -80,14 +66,14 @@ int PID(float error) {
   for(int i = 0; i<3; i++) {
     dBuffer[i] = dBuffer[i+1];
   }
-  dBuffer[3] = error-dBuffer[2];
+  dBuffer[3] = error;
   int average = 0;
   for(int i = 0; i<4; i++) {
     average = average + dBuffer[i];
   }
   average = average/4;
-  float dWeight = average*dGain;
-  return (int)round(pWeight);//(int)round(pWeight + iWeight + dWeight);
+  float dWeight = (average-error)*dGain;
+  return (int)round(pWeight + iWeight + dWeight);
 }
 
 void serialEvent()
@@ -98,9 +84,7 @@ void serialEvent()
     Serial.readBytes(buf,2);
     data =  word(buf[1],buf[0]);
     if (data == 5555)
-    {
       dataPiece = 0;
-    }
     if (dataPiece == 1)
       xTarget = data;
     if (dataPiece == 2)
@@ -111,9 +95,18 @@ void serialEvent()
       yLoc = data;
     if (dataPiece == 5)
       isBallOnTable = data;
+    if (dataPiece == 6)
+      pGain = (float)data/1000;
+    if (dataPiece == 7)
+      iGain = (float)data/1000;
+    if (dataPiece == 8)
+    {
+      dGain = (float)data/1000;
+      BoolNewData = 1;
+    }
       
     dataPiece = dataPiece+1;
-    writeInt(xTarget);
+    writeInt(data);
   }
   
 }
@@ -127,33 +120,3 @@ void writeInt(int data)
       Serial.write(highByte(data));
     }
 }
-
-void writeDisplay()
-{
-  lc.clearDisplay(0);
-  lc.clearDisplay(1);
-  writeNumber(1,7,xLoc);
-  writeNumber(1,3,yLoc);
-  writeNumber(0,7,xTarget);
-  writeNumber(0,3,yTarget);
-}
-
-void writeNumber(int board, int loc, int x)
-{
-  int ones;
-  int tens;
-  int hundreds;
-  int thousands;
-  ones = x%10;
-  x = x/10;
-  tens = x%10;
-  x = x/10;
-  hundreds = x%10;
-  x = x/10;
-  thousands = x;
-  lc.setChar(board,loc,thousands,false);
-  lc.setChar(board,loc-1,hundreds,false);
-  lc.setChar(board,loc-2,tens,false);
-  lc.setChar(board,loc-3,ones,false);
-}
-  
